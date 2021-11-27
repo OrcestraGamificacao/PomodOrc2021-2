d716e7d7355586b15323c39556a092d842f944d0
Victor Eduardo <victorear05@gmail.com>
2021-11-27 16:41:10 -0300

    navBar e inicio da Home

--- front/src/components/NavBar/index.tsx
+++ front/src/components/NavBar/index.tsx
@@ -1,12 +1,34 @@
 import "./style.css"
+import { ReactComponent as Tarefa} from "../../assets/tarefas.svg"
+import { ReactComponent as Ampulheta} from "../../assets/ampulheta.svg"
+import { ReactComponent as Feedback} from "../../assets/feedback.svg"
+import { ReactComponent as Ajuda} from "../../assets/ajuda.svg"
+
+
 const NavBar = () => {
     return(
         <section className="navBarContainer">
             <section className="navBarItemContainer">
-               <div>asdfasdf</div> 
-               <div>asdfasdf</div>
-               <div>asdfasdf</div>
-               <div>asdfasdf</div>
+                <a href="">
+                    <div className="navBarItem">
+                        <Tarefa fill="#DADDFC" width="70%" height="45%"/>
+                    </div>
+                </a>
+                <a href="">
+                    <div className="navBarItem">
+                        <Ampulheta fill="#DADDFC" width="70%" height="70%"/>
+                    </div>
+                </a>
+                <a href="">
+                    <div className="navBarItem">
+                        <Feedback fill="#DADDFC" width="70%" height="70%"/>
+                    </div>
+                </a>
+                <a href="">
+                    <div className="navBarItem">
+                        <Ajuda fill="#DADDFC" width="70%" height="70%"/>
+                    </div>
+                </a>
             </section>
         </section>
     )
--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -1,9 +1,16 @@
 import NavBar from "../../components/NavBar"
+import "./style.css"
 
 const Home = () => {
     return(
         <div>
             <NavBar />
+            <section className="homeContainer">
+            
+            
+            
+            </section>  
+            
         </div>
     )
 }
