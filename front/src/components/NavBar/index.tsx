a804b49cba304282d715ce7f6f3fec3e12bbbf17
Victor Eduardo <victorear05@gmail.com>
2021-11-27 11:11:56 -0300

    importando css

---
+++ front/src/components/NavBar/index.tsx
@@ -0,0 +1,15 @@
+import "./style.css"
+const NavBar = () => {
+    return(
+        <section className="navBarContainer">
+            <section className="navBarItemContainer">
+               <div>asdfasdf</div> 
+               <div>asdfasdf</div>
+               <div>asdfasdf</div>
+               <div>asdfasdf</div>
+            </section>
+        </section>
+    )
+}
+
+export default NavBar
--- front/src/components/navBar.tsx
+++
@@ -1,9 +0,0 @@
-const NavBar = () => {
-    return(
-        <div className="navBarCoitainer">
-             
-        </div>
-    )
-}
-
-export default NavBar
